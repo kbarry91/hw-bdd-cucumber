00b49c8b2ce940daecb38d2bff58a941cab3935b
kbarry91 <g00339811@gmit.ie>
2019-03-08 19:18:28 +0000

    Passing ratings test

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create(movie)
+    Movie.create!(movie)
   end
   #fail "Unimplemented"
 end
@@ -19,7 +19,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -30,9 +30,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  # fail "Unimplemented"
+  
+  # Split rating_list at the comma and iterate result
+  rating_list.split(', ').each do |rating|
+    # if unchecked uncheck rating
+    if uncheck
+      uncheck("ratings[#{rating}]")
+    else 
+      check("ratings[#{rating}]")
+    end
+  end
 end
-
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
   fail "Unimplemented"
